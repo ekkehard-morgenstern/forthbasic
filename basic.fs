6688b7a84f4fbbade284763a16b5e9852efc53c9
Ekkehard Morgenstern <ekkehard@ekkehardmorgenstern.de>
2019-07-08 20:23:18 +0200

    Properly GNU-ified

--- basic.fs
+++ basic.fs
@@ -1,7 +1,22 @@
 
-\ Forth BASIC v0.1 - Copyright (c) 2019 Ekkehard Morgenstern. All rights reserved. 
-\ Licensable under the GNU General Public License (GPL) v3 or higher. 
-\ Written for use with GNU Forth (aka GForth). 
+\ Forth BASIC v0.1 - A BASIC interpreter written in FORTH.
+\ Copyright (C) 2019 Ekkehard Morgenstern
+\ 
+\ This program is free software: you can redistribute it and/or modify
+\ it under the terms of the GNU General Public License as published by
+\ the Free Software Foundation, either version 3 of the License, or
+\ (at your option) any later version.
+\ 
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+\ 
+\ You should have received a copy of the GNU General Public License
+\ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+\ 
+\ This version has been written for use with GNU Forth (aka GForth). 
+\ Contact info: ekkehard@ekkehardmorgenstern.de
 
 \ === CONSTANTS =========================================================================
 
