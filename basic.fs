7f8bbec4a8225d414e133cd0d6d3c1c410e1fb96
Ekkehard Morgenstern <ekkehard@ekkehardmorgenstern.de>
2019-07-08 20:33:21 +0200

    Added proper licensing info

--- basic.fs
+++ basic.fs
@@ -15,6 +15,9 @@
 \ You should have received a copy of the GNU General Public License
 \ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 \ 
+\ Programs written in Forth BASIC do not fall under this license except 
+\ where otherwise noted.
+\ 
 \ This version has been written for use with GNU Forth (aka GForth). 
 \ Contact info: ekkehard@ekkehardmorgenstern.de
 
